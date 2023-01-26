748348a35d2e2558376cd483fd83b77b3fa544d1
nakamura yugo <runworld1919@gmail.com>
2023-01-26 22:28:21 +0900

    routesの修正、pathの記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/edit'
+  root to: "tasks#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :tasks
-
-  root to: "tasks#index"
 end
